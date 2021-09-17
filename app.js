855f586ad2b4014d58f780dda895cd00a9e18886
Matt Green <81880241+migreen92@users.noreply.github.com>
2021-09-17 15:13:26 -0400

    Finished JS, CSS Transition

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
+// *********** VARIABLES *********** //
 
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const resetButton = document.querySelector('a');
-const missed = 0;
-
-const randomPhrase = [
+const phrases = [
   'But February made me shiver',
   'With every paper I delivered',
   'Bad news on the doorstep',
@@ -13,59 +9,144 @@ const randomPhrase = [
 ];
 
 
-// Start game button hides the overlay
-resetButton.addEventListener('click', (e) => {
-  const displayNone = e.target.parentNode;
-  overlay.style.display = "none";
-});
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startGame = document.querySelectorAll(".btn__reset")[0];
+const overlay = document.querySelector("#overlay");
+let missed = 0;
 
-// selects one of the five strings within randomPhrase
-// splits the array into individual characters
-function getRandomPhrasesAsArray(phrasesArray) {
-  const random = phrasesArray[Math.floor(Math.random()*phrasesArray.length)];
-  let splitArray = random.split('');
-  return splitArray;
+// *********** FUNCTIONS *********** //
+
+
+function getRandomPhraseAsArray(arr) {
+    var a =  arr[Math.floor(Math.random() * arr.length)];
+    const b = [];
+
+    for (let i = 0; i < a.length; i ++) {
+        b.push(a.charAt(i));
+    }
+
+    return b
 }
 
-// uses the argument from getRandomPhrasesAsArray
-// creates a <li> for each element created by random.split('')
-// if ' ' = space, otherwise, letter
-function addPhrasetoDisplay(phrasesArray) {
-  for (i = 0; i < phrasesArray.length; i++) {
-    const ul = document.getElementById('phrase');
-    const li = document.createElement('li');
-    ul.appendChild(li);
-      if (phrasesArray[i] != ' ') {
-        li.className = 'letter'
-      } else {
-        li.className = 'space'
-      }
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+// PUTS PHRASE ON THE VIEWPOINT
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < arr.length; i ++) {
+    const letter = document.querySelector("#phrase ul");
+    const li = document.createElement("li");
+
+    letter.appendChild(li);
+    li.textContent = arr[i].toUpperCase();
+
+    if (li.textContent !== " ") {
+        li.className = "letter"
+
+    } else {
+        li.className = "space"
+    }
+  }
+}
+
+
+// HIDES OVERLAY SCREEN
+startGame.addEventListener('click', () => {
+    overlay.style.display = 'none';
+
+    // Reset the game
+
+        // Resets the phrase to guess
+    const letter = document.querySelector('#phrase ul');
+    letter.textContent = '';
+
+        // Resets styling of buttons to default
+    const resetKeyboard = document.querySelectorAll('.keyrow button');
+
+    for (let i = 0; i < resetKeyboard.length; i += 1) {
+        resetKeyboard[i].className = ' ';
+        resetKeyboard[i].removeAttribute('disabled');
+    }
+
+        // Resets hearts images to default (5 lifes)
+
+    const tries = document.querySelectorAll('.tries');
+
+    for (let i = 0; i < tries.length; i += 1) {
+        tries[i].firstChild.setAttribute('src','images/liveHeart.png');
+    }
+
+        // Reset missed attempts score
+
+        missed = 0;
+
+    // SETS PHRASE TO GUESS AND REMOVES TRANSITION PROPERTY
+    phrase.style.display = 'flex';
+    let a = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(a);
+
+});
+
+
+function checkLetter (chosenLetter) {
+    const selectedLetter = chosenLetter.toUpperCase();
+    const letters = document.querySelectorAll('.letter');
+    let letterFound = 'null';
+
+    for (let i = 0; i < letters.length; i += 1) {
+
+         if (selectedLetter === letters[i].textContent) {
+            letterFound = selectedLetter;
+            letters[i].className += ' show flip';
+        }
+    }
+    return letterFound
+}
+
+
+function checkWin() {
+    const letters = document.querySelectorAll('.letter');
+    const flip = document.getElementById('#phrase');
+    const shownLetters = document.querySelectorAll('.show');
+    const statusMessage = overlay.firstChild.nextElementSibling;
+    const ctaReplay = overlay.lastChild.previousElementSibling;
+
+    if (letters.length === shownLetters.length) {
+        overlay.style.display = 'flex';
+        overlay.className = 'win';
+        statusMessage.textContent = 'YOU WON!!!';
+        ctaReplay.textContent = 'Play Again';
+        phrase.style.display = 'none';
+    }
+
+    if (missed >= 5) {
+        overlay.style.display = 'flex';
+        overlay.className = 'lose';
+        statusMessage.textContent = "You'll get it next time!";
+        ctaReplay.textContent = 'Play Again';
+        phrase.style.display = 'none';
     }
 }
 
-// variable to create the split of a random array
-// argument gets saved in phrases and run through the addPhrasetoDisplay function
-const phrases = getRandomPhrasesAsArray(randomPhrase);
-addPhrasetoDisplay(phrases);
+/// GAMEPLAY EVENT STARTER
 
-// sets button as the argument
-// default state is false/null
-// if button's text content matches the letter, class show is added
-function checkLetter (button){
-  const clicked = false;
-  const letter = document.querySelector('li');
-    for (let i = 0; i < letter.length; i++) {
-      if(button.target.textcontent === letter[i].textcontent) {
-        letter[i].classList.add('show');
-        clicked = true;
-      }
-    return clicked;
-  }
-}
-qwerty.addEventListener('click', (e) => {
-  let letterFound = checkLetter(event);
-  if (event.target === 'BUTTON') {
-    event.target.classList = 'chosen';
-    event.target.disabled = true;
-  }
-})
+qwerty.addEventListener('click', (event) => {
+
+    const a = event.target;
+    const b = a.textContent;
+
+    if (a.tagName == 'BUTTON') {
+        a.className = 'chosen';
+        a.setAttribute('disabled', true);
+        const c = checkLetter(b);
+
+        if (c === 'null') {
+            a.className += ' chosen_mistaken';
+            const tries = document.querySelectorAll('.tries');
+            tries[missed].firstChild.setAttribute('src','images/lostHeart.png');
+            missed += 1;
+        }
+    }
+    checkWin();
+});
